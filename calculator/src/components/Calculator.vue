e3493cb8f280e88067eb122c9ac81bbad047aa9d
Ken <kencho.gigascience@gmail.com>
2021-08-31 10:30:50 +0800

    Add operators logic

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -27,7 +27,9 @@
 export default {
   data() {
     return {
+      previous: null,
       current: '',
+      operator: null,
     }
   },
   methods: {
@@ -42,6 +44,10 @@ export default {
       this.current = `${parseFloat(this.current) / 100}`
     },
     append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
       this.current = `${this.current}${number}`;
     },
     dot() {
@@ -49,20 +55,32 @@ export default {
         this.append('.');
       }
     },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
     divide() {
-
+      this.operator = (a, b) => b / a;
+      this.setPrevious();
     },
     times() {
-
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
     },
     minus() {
-
+      this.operator = (a, b) => b - a;
+      this.setPrevious();
     },
     add() {
-
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
     },
     equal() {
-
+      this.current = `${this.operator(
+          parseFloat(this.current),
+          parseFloat(this.previous)
+      )}`;
+      this.previous = '';
     }
   }
 }
