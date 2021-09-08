2923e13d7b46477de285e73cc72b3350365acc8e
Ken <kencho.gigascience@gmail.com>
2021-09-08 16:38:15 +0800

    Create todo app folder

---
+++ todo-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ todo-app/src/components/Todo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+    <!-- Input -->
+    <div class="d-flex">
+      <input type="text" placeholder="Enter task" class="form-control">
+      <button class="btn btn-warning rounded-0">SUBMIT</button>
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+      <tr>
+        <th scope="col">Task</th>
+        <th scope="col">Status</th>
+        <th scope="col" class="text-center">#</th>
+        <th scope="col" class="text-center">#</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Steal bananas</td>
+        <td>To-do</td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-pen"></span>
+          </div>
+        </td>
+        <td>
+          <div class="text-center">
+            <span class="fa fa-trash"></span>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
